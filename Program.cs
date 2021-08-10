0cb38a6b0a08cf896743a6c8e564354515c5b297
Ovis <me+github@hitsujin.jp>
2021-08-11 00:33:09 +0900

    OEmbedShortCodeの仮

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BlogGenerator.ShortCodes;
 using Statiq.App;
 using Statiq.Web;
 
 namespace BlogGenerator
 {
-  public class Program
+    public class Program
   {
     public static async Task<int> Main(string[] args) =>
       await Bootstrapper
         .Factory
         .CreateWeb(args)
         .AddShortcode<AmazonAffiliateShortCodes>("AmazonAffiliate")
+        .AddShortcode<OEmbedShortCodes>("EmbedLink")
         .RunAsync();
   }
 }
---
+++ ShortCodes/OEmbedShortCodes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Statiq.Common;
+
+namespace BlogGenerator.ShortCodes
+{
+    class OEmbedShortCodes: SyncShortcode
+    {
+        private const string Url = nameof(Url);
+
+  
+        public override ShortcodeResult Execute(KeyValuePair<string, string>[] args, string content, IDocument document, IExecutionContext context)
+        {
+            IMetadataDictionary arguments = args.ToDictionary(Url);
+            arguments.RequireKeys(Url);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"<a href=\"")
+                .AppendLine($"{arguments.GetString(Url)}")
+                .AppendLine($"\" target=\"_blank\">")
+                .AppendLine($"{arguments.GetString(Url)}")
+                .AppendLine($"</a>");
+
+            return sb.ToString();
+        }
+
+    }
+}
