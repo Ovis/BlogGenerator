b2f4c52e6d6920e162d0b2a6779b505daa4c8f24
Ovis <me@hitsujin.jp>
2025-04-28 15:53:09 +0900

    RSS設定

---
+++ src/Models/FeedOption.cs
@@ -0,0 +1,34 @@
+﻿namespace BlogGenerator.Models;
+
+public class FeedOption
+{
+    /// <summary>
+    /// RSS2.0フィードを生成するかどうか
+    /// </summary>
+    public bool UseRss2 { get; set; } = true;
+
+    /// <summary>
+    /// フィードのファイル名（RSS）
+    /// </summary>
+    public string RssFileName { get; set; } = "feed.rss";
+
+    /// <summary>
+    /// Atomフィードを生成するかどうか
+    /// </summary>
+    public bool UseAtom { get; set; } = true;
+
+    /// <summary>
+    /// フィードのファイル名（Atom）
+    /// </summary>
+    public string AtomFileName { get; set; } = "feed.atom";
+
+    /// <summary>
+    /// フィードに含める記事の最大数
+    /// </summary>
+    public int MaxFeedItems { get; set; } = 10;
+
+    /// <summary>
+    /// フィードの言語
+    /// </summary>
+    public string Language { get; set; } = "ja-JP";
+}
