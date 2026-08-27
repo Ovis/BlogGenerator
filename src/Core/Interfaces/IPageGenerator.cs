8b3df75ef81e072c9c0c4e662bbe10e3d9619121
Ovis <3971624+Ovis@users.noreply.github.com>
2026-08-28 00:18:40 +0900

    Expose scheduled page validation

--- src/Core/Interfaces/IPageGenerator.cs
+++ src/Core/Interfaces/IPageGenerator.cs
@@ -1,10 +1,11 @@
-﻿using BlogGenerator.Models;
+using BlogGenerator.Models;
 
 namespace BlogGenerator.Core.Interfaces;
 
 public interface IPageGenerator
 {
     Task<TrustedHtml> GenerateSideBarHtmlAsync(List<Article> articles);
+    Task ValidateArticlePageAsync(Article article, List<Article> publishedArticles, TrustedHtml sideBarHtml);
     Task GenerateArticlePagesAsync(List<Article> articles, string outputDir, TrustedHtml sideBarHtml);
     Task GenerateIndexPagesAsync(List<Article> articles, string outputDir, TrustedHtml sideBarHtml);
     Task GenerateTagPagesAsync(List<Article> articles, string outputDir, TrustedHtml sideBarHtml);
