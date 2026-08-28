7942b8600c60b3ec403872ef37e080786b0a8d0b
Ovis <3971624+Ovis@users.noreply.github.com>
2026-08-28 19:39:36 +0900

    パス検証テストのファイル名を整理

---
+++ tests/BlogGenerator.Tests/DirectoryPathValidatorTests.cs
@@ -0,0 +1,29 @@
+using BlogGenerator.Core;
+using NUnit.Framework;
+
+namespace BlogGenerator.Tests;
+
+[TestFixture]
+public class DirectoryPathValidatorTests
+{
+    [Test]
+    public void outputがinput配下なら例外を送出する()
+    {
+        var inputDir = Path.Combine(Path.GetTempPath(), "BlogGenerator.Tests", Guid.NewGuid().ToString("N"), "input");
+        var outputDir = Path.Combine(inputDir, "output");
+
+        Assert.Throws<ArgumentException>(() =>
+            DirectoryPathValidator.ThrowIfOutputDirectoryIsInputSubdirectory(inputDir, outputDir));
+    }
+
+    [Test]
+    public void outputがinput配下でなければ例外を送出しない()
+    {
+        var rootDir = Path.Combine(Path.GetTempPath(), "BlogGenerator.Tests", Guid.NewGuid().ToString("N"));
+        var inputDir = Path.Combine(rootDir, "input");
+        var outputDir = Path.Combine(rootDir, "output");
+
+        Assert.DoesNotThrow(() =>
+            DirectoryPathValidator.ThrowIfOutputDirectoryIsInputSubdirectory(inputDir, outputDir));
+    }
+}
